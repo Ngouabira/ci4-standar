abc1c5c573fab77cd0b87ed3cd13807129169ea3
Valdy NGOUABIRA <valdyngouabira@gmail.com>
2023-10-30 07:49:39 +0000

    wip

--- app/Views/admin/user/create.php
+++ app/Views/admin/user/create.php
@@ -9,9 +9,6 @@
             <div class="col-10 mt-2">
                 <h5 class="card-title text-primary"><?=lang('user.new_user')?></h5>
             </div>
-            <div class="col-2">
-                <button type="submit" class="btn float-right btn-primary" href="user/new" title="<?=translate("user.App_new")?>"> <i class="icon-copy fi-save"></i> </button>
-            </div>
         </div>
     </div>
     <!-- /.card-header -->
@@ -28,35 +25,34 @@
                 <input type="email" name="email" value="<?=old('email')?>" id="email" class="form-control <?=session('errors.email') ? 'is-invalid' : ''?>">
             </div>
             <div class="col-12 pt-1">
-                <label for="roles" class="col-sm-2 col-form-label"><?=lang('user.roles')?></label>
-                <div class="col-sm-8">
-                    <select class="custom-select2 form-control select2-hidden-accessible" name="roles[]" multiple="multiple" data-placeholder="<?=lang('user.roles')?>" style="width: 100%;">
-                        <?php foreach ($roles as $role) {?>
-                            <option <?=in_array($role['id'], old('roles', [])) ? 'selected' : ''?> value="<?=$role['id']?>"><?=$role['name']?></option>
-                        <?php }?>
-                    </select>
-                    <?php if (session('error.role')) {?>
-                        <h6 class="text-danger"><?=session('error.role')?></h6>
+                <label for="roles" class="col-form-label"><?=lang('user.roles')?></label>
+                <select class="custom-select2 form-control select2-hidden-accessible" name="roles[]" multiple="multiple" data-placeholder="<?=lang('user.roles')?>" style="width: 100%;">
+                    <?php foreach ($roles as $role) {?>
+                        <option <?=in_array($role['id'], old('roles', [])) ? 'selected' : ''?> value="<?=$role['id']?>"><?=$role['name']?></option>
                     <?php }?>
-                </div>
+                </select>
+                <?php if (session('error.role')) {?>
+                    <h6 class="text-danger"><?=session('error.role')?></h6>
+                <?php }?>
             </div>
             <div class="col-12 pt-1">
-                <label for="permissions" class="col-sm-2 col-form-label"><?=lang('user.permissions')?></label>
-                <div class="col-sm-8">
-                    <select class="custom-select2 form-control select2-hidden-accessible" name="permissions[]" multiple="multiple" data-live-search="true" data-placeholder="<?=lang('user.permissions')?>" style="width: 100%;">
-                        <?php foreach ($permissions as $permission) {?>
-                            <option <?=in_array($permission['id'], old('permissions', [])) ? 'selected' : ''?> value="<?=$permission['id']?>"><?=$permission['name']?></option>
-                        <?php }?>
-                    </select>
-                    <?php if (session('error.permission')) {?>
-                        <h6 class="text-danger"><?=session('error.permission')?></h6>
+                <label for="permissions" class="col-form-label"><?=lang('user.permissions')?></label>
+                <select class="custom-select2 form-control select2-hidden-accessible" name="permissions[]" multiple="multiple" data-live-search="true" data-placeholder="<?=lang('user.permissions')?>" style="width: 100%;">
+                    <?php foreach ($permissions as $permission) {?>
+                        <option <?=in_array($permission['id'], old('permissions', [])) ? 'selected' : ''?> value="<?=$permission['id']?>"><?=$permission['name']?></option>
                     <?php }?>
-                </div>
+                </select>
+                <?php if (session('error.permission')) {?>
+                    <h6 class="text-danger"><?=session('error.permission')?></h6>
+                <?php }?>
             </div>
 
             <div class="col-12 pt-1">
                 <label class="form-label" for="password"><?=lang('user.password')?></label>
                 <input type="password" name="password" id="password" class="form-control <?=session('errors.password') ? 'is-invalid' : ''?>">
+            </div>
+            <div class="pt-2 col-12">
+                <button type="submit" id="create" class="btn btn-primary"><?=lang('base.create')?></button>
             </div>
         </form>
     </div>
