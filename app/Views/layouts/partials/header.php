144aeaa0aa108fc9296520e7b3fb25c630aa8e21
NGOUABIRA <v.ngouabira@iatf.it>
2023-11-01 16:27:35 +0100

    wip

--- app/Views/layouts/partials/header.php
+++ app/Views/layouts/partials/header.php
@@ -3,32 +3,33 @@
 		<div class="menu-icon dw dw-menu"></div>
 	</div>
 
-	<div class="user-notification">
+	<div class="header-right">
+		<div class="user-notification">
 			<div class="dropdown">
 				<a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
 
-				<i style="font-size: 24px;  vertical-align: text-bottom;" class="icon-copy fi-web dw-notification"></i>
+					<i style="font-size: 24px;  vertical-align: text-bottom;" class="icon-copy fi-web dw-notification"></i>
 
-					<span class=""><?=translate('base.' . session()->get("country")) ?? translate('base.french')?></span>
+					<span class=""><?= translate('base.' . session()->get("country")) ?? translate('base.french') ?></span>
 				</a>
 				<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
-					<a class="dropdown-item" href="/translate/fr"><?=translate('base.french')?></a>
-					<a class="dropdown-item" href="/translate/en"><?=translate('base.english')?></a>
+					<a class="dropdown-item" href="/translate/fr"><?= translate('base.french') ?></a>
+					<a class="dropdown-item" href="/translate/en"><?= translate('base.english') ?></a>
 				</div>
 			</div>
-	</div>
+		</div>
 
 		<div class="user-info-dropdown">
 			<div class="dropdown">
 				<a class="dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-expanded="false">
 					<span class="user-icon">
-						<img src="/uploads/<?=authUser()['image']?>" alt="">
+						<img src="/uploads/<?= authUser()['image'] ?>" alt="">
 					</span>
-					<span class="user-name"><?=authUser()['name']?></span>
+					<span class="user-name"><?= authUser()['name'] ?></span>
 				</a>
 				<div class="dropdown-menu dropdown-menu-right dropdown-menu-icon-list">
-					<a class="dropdown-item" href="/profile"><i class="dw dw-user1"></i> <?=translate('base.profile')?></a>
-					<a class="dropdown-item" href="/logout"><i class="dw dw-logout"></i> <?=translate('base.logout')?></a>
+					<a class="dropdown-item" href="/profile"><i class="dw dw-user1"></i> <?= translate('base.profile') ?></a>
+					<a class="dropdown-item" href="/logout"><i class="dw dw-logout"></i> <?= translate('base.logout') ?></a>
 				</div>
 			</div>
 		</div>
