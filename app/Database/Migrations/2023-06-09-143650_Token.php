8e31bdd8ea0ca0b171b68cee0e4cd98b9a11eaa2
NGOUABIRA <v.ngouabira@iatf.it>
2023-11-02 12:29:23 +0100

    wip

--- app/Database/Migrations/2023-06-09-143650_Token.php
+++ app/Database/Migrations/2023-06-09-143650_Token.php
@@ -9,13 +9,19 @@ class Token extends Migration
     public function up()
     {
         $this->forge->addField([
+            'id' => [
+                'type' => 'INT',
+                'constraint' => 11,
+                'unsigned' => true,
+                'auto_increment' => true,
+            ],
             'user_id' => ['type' => 'int', 'constraint' => 11, 'unsigned' => true, 'default' => 0],
-            'token' => ['type' => 'string', 'constraint' => 255],
+            'token' => ['type' => 'varchar', 'constraint' => 255, 'default' => null],
             'created_at' => ['type' => 'datetime', 'null' => true, 'default' => null],
             'expire_at' => ['type' => 'datetime', 'null' => true, 'default' => null],
         ]);
 
-        $this->forge->addKey(['user_id']);
+        $this->forge->addKey(['id', 'user_id']);
         $this->forge->addForeignKey('user_id', 'user', 'id', '', 'CASCADE');
         $this->forge->createTable('token');
     }
